6710b595392906916601b2bd4048c86fe0bd9481
Mathew Grabau <mgrabau@mathewgrabau.ca>
2018-01-17 23:25:35 -0600

    Got the tasks being inserted into the array, binding and showing the collection of them

--- WebContent/javascript/todolist.js
+++ WebContent/javascript/todolist.js
@@ -1,11 +1,28 @@
 var ToDoList = function() {
-	var init = function() {
+	var task = {
+		name: ko.observable(),
+		description: ko.observable()
+	};
 
+	var tasks = ko.observableArray();
+
+	var addTask = function() {
+		console.log("addTask: name: " + task.name() + "; description: " + task.description());
+		tasks.push({
+			name: task.name(),
+			description: task.description()
+		});
+	};
+
+	var init = function() {
+		ko.applyBindings(ToDoList);
 	};
 
 	$(init);
 
 	return {
-
+		tasks: tasks,
+		task: task,
+		addTask: addTask
 	};
 }();
